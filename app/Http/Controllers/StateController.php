9c790902bba2f66e343bbc3ca9635babe1ff7761
Arthur Soares Parente <arthur.parente@tridia.com.br>
2025-02-03 17:03:30 -0300

    :sparkles: feat: Controller de State

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StateStoreRequest;
+use App\Http\Requests\StateUpdateRequest;
 use NunoMaduro\Collision\Adapters\Phpunit\State;
 
 class StateController extends Controller
@@ -13,7 +15,7 @@ class StateController extends Controller
      */
     public function index()
     {
-        $states = State::paginate(10);
+        $states = State::paginate();
         
         return $states;
     }
@@ -23,30 +25,46 @@ class StateController extends Controller
      */
     public function store(StateStoreRequest $request)
     {
-        
+        $state = DB::transaction(function() use ($request){
+         $state = State::create($request->validated());
+        }); 
+
+        return $state;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(State $state)
     {
-        //
+        return $state->load();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StateUpdateRequest $request, State $state)
     {
-        //
+        $state->update($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'data' => $state,
+            'message' => 'State atualizado com sucesso.'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(State $state)
     {
-        //
+        $state->delete();
+
+        // Retornar resposta de sucesso
+        return response()->json([
+            'success' => true,
+            'message' => 'State deletado com sucesso.'
+        ], 200);
     }
 }
